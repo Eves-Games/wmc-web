8829e3dec4b316faea313ee37176168622dfd782
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-07-27 00:37:16 +1000

    environment type abstraction

---
+++ src/environment.ts
@@ -0,0 +1,18 @@
+type Environment = 'production' | 'preview' | 'development';
+
+export const getBaseUrl = () => {
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+    return 'http://localhost:3000'
+}
+
+export function getEnvironment(): Environment {
+    if (process.env.VERCEL_ENV === 'production') {
+        return 'production';
+    }
+
+    if (process.env.VERCEL_ENV === 'preview') {
+        return 'preview';
+    }
+
+    return 'development';
+}
