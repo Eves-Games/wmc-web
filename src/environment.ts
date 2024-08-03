730c6de2b89ae7f00fd7450335a67629fa61d940
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-04 00:18:39 +1000

    Refactor environment functions and update type quotes
    Refactored the `getBaseUrl` and `getEnvironment` functions for better readability and consistency. Updated quotes from single to double for the `Environment` type definitions and returned constant values.

--- src/environment.ts
+++ src/environment.ts
@@ -1,18 +1,18 @@
-type Environment = 'production' | 'preview' | 'development';
+type Environment = "production" | "preview" | "development";
 
 export const getBaseUrl = () => {
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
-    return 'http://localhost:3000'
-}
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  return "http://localhost:3000";
+};
 
 export function getEnvironment(): Environment {
-    if (process.env.VERCEL_ENV === 'production') {
-        return 'production';
-    }
+  if (process.env.VERCEL_ENV === "production") {
+    return "production";
+  }
 
-    if (process.env.VERCEL_ENV === 'preview') {
-        return 'preview';
-    }
+  if (process.env.VERCEL_ENV === "preview") {
+    return "preview";
+  }
 
-    return 'development';
+  return "development";
 }
