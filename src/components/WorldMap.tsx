dc8d7a221d67771542a8fba7270754ca0ba3ea55
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-05 03:34:47 +1000

    Add WorldMap component to display an interactive map
    Introduce a new WorldMap component that embeds an interactive map using coordinates passed as props. The map is displayed in an iframe with styles applied for responsiveness and proper alignment.

---
+++ src/components/WorldMap.tsx
@@ -0,0 +1,18 @@
+interface WorldMapProps {
+  coordinates: {
+    x: number;
+    z: number;
+  };
+}
+
+export default function WorldMap({ coordinates }: WorldMapProps) {
+  return (
+    <div className="relative w-full overflow-hidden rounded-box pt-[56.25%] shadow">
+      <iframe
+        src={`https://map.worldmc.net/?world=minecraft_overworld&zoom=5&x=${coordinates.x}&z=${coordinates.z}`}
+        className="absolute left-0 top-0 h-full w-full"
+        allowFullScreen
+      />
+    </div>
+  );
+}
