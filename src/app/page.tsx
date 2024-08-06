5e9cc281f01369fd57f0c4a8a8178e6691e8f859
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-06 11:25:32 +1000

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import CopyIp from "@/components/CopyIp";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="space-y-4">
-      <div className="hero rounded-box bg-base-200 py-8 shadow-lg" style={{backgroundImage: "url(/londondark.png)", backgroundBlendMode: "multiply"}}>
+      <div
+        className="hero rounded-box bg-base-200 py-8 shadow-lg"
+        style={{ backgroundImage: "url(/londondark.png)", backgroundBlendMode: "multiply" }}
+      >
         <div className="hero-content text-center text-white">
           <div className="max-w-md">
             <h1 className="text-6xl font-black">WorldMC</h1>
