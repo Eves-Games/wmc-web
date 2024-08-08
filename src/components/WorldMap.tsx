536e9e70496ca6254e09f36d517bc6bbf565b373
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-09 00:51:30 +1000

    make coordinates param optional

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,5 +1,5 @@
 interface WorldMapProps {
-  coordinates: {
+  coordinates?: {
     x: number;
     z: number;
   };
@@ -9,7 +9,7 @@ export default function WorldMap({ coordinates }: WorldMapProps) {
   return (
     <div className="relative w-full overflow-hidden rounded-box pt-[56.25%] shadow">
       <iframe
-        src={`https://map.worldmc.net/?world=minecraft_overworld&zoom=5&x=${coordinates.x}&z=${coordinates.z}`}
+        src={`https://map.worldmc.net/?world=minecraft_overworld${coordinates && `&zoom=5&x=${coordinates.x}&z=${coordinates.z}`}`}
         className="absolute left-0 top-0 h-full w-full"
         allowFullScreen
       />
