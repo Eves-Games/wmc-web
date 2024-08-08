975d040a8b310f6f523478d6d667da9bae5ad459
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-09 03:06:46 +1000

    memoize item in a desperate attempt to get it to load quicker

--- src/components/minecraft/MinecraftItem.tsx
+++ src/components/minecraft/MinecraftItem.tsx
@@ -1,35 +1,43 @@
 "use client";
 
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useMemo } from "react";
 
 interface MinecraftItemProps {
   imageSrc: string;
   className?: string;
 }
 
-const MinecraftItem: React.FC<MinecraftItemProps> = ({ imageSrc, className = "" }) => {
+const MinecraftItem: React.FC<MinecraftItemProps> = React.memo(({ imageSrc, className = "" }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
+  const memoizedEffect = useMemo(() => {
+    return () => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
 
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
 
-    const img = new Image();
-    img.onload = () => {
-      canvas.width = img.width;
-      canvas.height = img.height;
+      const img = new Image();
+      img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-      ctx.imageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
 
-      ctx.drawImage(img, 0, 0, img.width, img.height);
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+      };
+      img.src = imageSrc;
     };
-    img.src = imageSrc;
   }, [imageSrc]);
 
+  useEffect(() => {
+    memoizedEffect();
+  }, [memoizedEffect]);
+
   return <canvas ref={canvasRef} className={className} style={{ imageRendering: "pixelated" }} />;
-};
+});
+
+MinecraftItem.displayName = "MinecraftItem";
 
 export default MinecraftItem;
