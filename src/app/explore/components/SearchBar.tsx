83bb11a4cfb6bb26a6d63d2d63d50faed18b608a
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-10 01:44:20 +1000

    fix bad width on mobile for search bar

--- src/app/explore/components/SearchBar.tsx
+++ src/app/explore/components/SearchBar.tsx
@@ -5,8 +5,6 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Search, ChevronDown, Flag, Building2, Users, ArrowDownAZ, Coins, LandPlot } from "lucide-react";
 import { useDebouncedCallback } from "use-debounce";
 
-const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
-
 export function SearchBar({ placeholder }: { placeholder: string }): JSX.Element {
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -30,12 +28,11 @@ export function SearchBar({ placeholder }: { placeholder: string }): JSX.Element
 
   return (
     <div className="flex items-center justify-center gap-2">
-      <label className="input input-bordered flex w-full items-center gap-4">
+      <label className="input input-bordered flex w-full items-center gap-2">
         <Search className="opacity-70" />
         <input
           type="text"
           name="search"
-          className="grow"
           placeholder={placeholder}
           defaultValue={searchParams.get("query")?.toString()}
           aria-label={placeholder}
@@ -43,8 +40,8 @@ export function SearchBar({ placeholder }: { placeholder: string }): JSX.Element
         />
       </label>
       <div className="dropdown dropdown-end flex-none">
-        <button tabIndex={0} className="btn m-1">
-          {currentFilter ? capitalize(currentFilter) : "Nations"} <ChevronDown />
+        <button tabIndex={0} className="btn">
+          {currentFilter || "Nations"} <ChevronDown />
         </button>
         <ul tabIndex={0} className="menu dropdown-content menu-sm z-[1] w-52 rounded-box bg-base-100 p-2 shadow">
           <li onClick={() => handleChange("filter", "Nations")}>
