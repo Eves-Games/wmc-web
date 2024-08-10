222c83cbd1a2c19024efde319ff0ed6593728578
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-10 13:26:19 +1000

    add zoom to param

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -3,13 +3,19 @@ interface WorldMapProps {
     x: number;
     z: number;
   };
+  zoom: number;
 }
 
-export default function WorldMap({ coordinates }: WorldMapProps) {
+export default function WorldMap({ coordinates, zoom }: WorldMapProps) {
+  coordinates = {
+    x: coordinates?.x || 0,
+    z: coordinates?.z || 0,
+  };
+
   return (
     <div className="relative w-full overflow-hidden rounded-box pt-[56.25%] shadow">
       <iframe
-        src={`https://map.worldmc.net/?world=minecraft_overworld${coordinates && `&zoom=5&x=${coordinates.x}&z=${coordinates.z}`}`}
+        src={`https://map.worldmc.net/?world=minecraft_overworld&zoom=${zoom}&x=${coordinates.x}&z=${coordinates.z}`}
         className="absolute left-0 top-0 h-full w-full"
         allowFullScreen
       />
